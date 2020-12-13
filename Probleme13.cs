86b2c67fafee6ff25bb1c0751da49ce302966ec8
kikoho <34505974+kikoho@users.noreply.github.com>
2020-12-13 23:10:54 +0100

    Create Probleme13.cs

---
+++ Probleme13.cs
@@ -0,0 +1,199 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using ConsoleApplication2.MathBase;
+
+namespace ConsoleApplication2
+{
+    public class bus
+    {
+        public int startTime;
+        public bool unactive  = false;
+        public bus(string t)
+        {
+            if(t == "x")
+            {
+                unactive = true;
+            }
+            else
+            {
+                startTime = Convert.ToInt32(t);
+            }
+        }
+
+
+
+    }
+
+    public class day13
+    {
+
+        static List<string> readInput()
+        {
+            List<string> inputs = new List<string>();
+            for (int t = 0; t < 5000000; t++)
+            {
+                string text = "";
+                try
+                {
+                    text = getS();
+                }
+                catch
+                {
+                    break;
+                }
+                inputs.Add(text);
+            }
+            return inputs;
+        }
+
+        static long myAlgo()
+        {
+
+            int timeStart = getI();
+            string t = getS();
+
+            string[] all = t.Split(',');
+
+            List<int> allBusStart = new List<int>();
+            List<bus> allBus = new List<bus>();
+
+            for (int i = 0; i < all.Length; i++)
+            {
+                allBus.Add(new bus(all[i]));
+            }
+
+            
+            long bestDif = 50000000;
+
+            long factCurrent = allBus[0].startTime;
+
+            long res = 0;
+
+            for (int j = 1; j < allBus.Count; j++)
+            {
+                bus b = allBus[j];
+                if (b.unactive)
+                    continue;
+
+
+                while (true)
+                {
+                    if (b.startTime - (res % b.startTime) == j % b.startTime)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        res += factCurrent;
+                    }
+                }
+                factCurrent = factCurrent * b.startTime / BaseMath.PGCD(factCurrent, b.startTime);
+
+
+                factCurrent *= 1;
+
+
+
+
+                /*if(b.unactive)
+                    continue;
+                int waitTime = b.startTime - timeStart % b.startTime;
+                if(waitTime < bestDif)
+                {
+                    res = waitTime * b.startTime;
+                    bestDif = waitTime;
+                }*/
+            }
+
+            List<long> modilo = new List<long>();
+            for (int j = 0; j < allBus.Count; j++)
+            {
+                bus b = allBus[j];
+                if (b.unactive)
+                    continue;
+
+                modilo.Add(res % b.startTime);
+
+            }
+
+
+
+
+            for (int j = 0; j < allBus.Count; j++)
+            {
+            }
+            for (int k = 0; k < allBus.Count; k++)
+            {
+            }
+
+            return res;
+
+        }
+
+        static void result()
+        {
+            line = GetTestDebug();
+            long resDebug = myAlgo();
+
+            line = GetTest();
+            long resFinal = myAlgo();
+            resFinal = 0;
+        }
+
+
+
+
+        #region main
+        public static string getS()
+        {
+            return nextLine();
+        }
+        static int currentLine = -1;
+        static List<string> line;
+        public static string nextLine()
+        {
+            currentLine++;
+            return line[currentLine];
+        }
+        public static int getI()
+        {
+            return Convert.ToInt32(nextLine());
+        }
+        public static List<string> GetTestDebug()
+        {
+            currentLine = -1;
+            List<string> lines = new List<string>();
+
+            StreamReader file = new System.IO.StreamReader(@"C:\Users\Henri\Desktop\CodeChallenge\InputTest.txt");
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+            file.Close();
+            return lines;
+        }
+        public static List<string> GetTest()
+        {
+            currentLine = -1;
+            List<string> lines = new List<string>();
+
+            StreamReader file = new System.IO.StreamReader(@"C:\Users\Henri\Desktop\CodeChallenge\Input.txt");
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+            file.Close();
+            return lines;
+        }
+        public static void Main()
+        {
+            result();
+        }
+        #endregion
+    }
+}
